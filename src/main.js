39c39bcc40a6e7db289e994b7b676b9dcaa15ada
Gibran Ojeda <16161306@itoaxaca.edu.mx>
2021-11-13 12:40:16 -0600

    Notifications
    Notificaciones en Vuejs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueNativeNotification from 'vue-native-notification'
+ 
+Vue.use(VueNativeNotification, {
+  // Automatic permission request before
+  // showing notification (default: true)
+  requestOnNotify: true
+})
 
 Vue.config.productionTip = false
 
